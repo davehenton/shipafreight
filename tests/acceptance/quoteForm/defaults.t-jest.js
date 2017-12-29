ab62e9aaf579ef071ff4f7de9c053ad615d22576
James Burgess <jamesmillerburgess@gmail.com>
2017-12-29 14:14:35 +0100

    Enable the rest of the tests

--- tests/acceptance/quoteForm/defaults.t-jest.js
+++ tests/acceptance/quoteForm/defaults.t-jest.js
@@ -15,82 +15,88 @@ describe('Quote Form Defaults', function() {
     const classes = await b.getAttribute('#ocean-button', 'class');
     expect(/active/.test(classes)).toBe(true);
   });
-  // it('defaults movement type to Door to Door', () => {
-  //   const movementType = b.element('.movement-type .Select-value-label span');
-  //   expect(movementType.getText()).toBe('Door to Door');
-  // });
-  // it('defaults a single package row', () => {
-  //   const cargoRows = b.elements('.package-row');
-  //   expect(cargoRows.value.length).toBe(1);
-  // });
-  // it('defaults package type to Packages', () => {
-  //   const packageType = b.element('.package-type .Select-value-label span');
-  //   expect(packageType.getText()).toBe('Packages');
-  // });
-  // it('defaults quantity to 1', () => {
-  //   const quantity = b.element('.quantity input');
-  //   expect(quantity.getValue()).toBe('1');
-  // });
-  // it('defaults to using dimensions', () => {
-  //   const dimensionsCheckbox = b.element('.dimensions .checkbox');
-  //   expect(/active/.test(dimensionsCheckbox.getAttribute('class'))).toBe(true);
-  // });
-  // it('defaults dimensions UOM to cm', () => {
-  //   const dimensionsUOM = b.element('.dimensions-uom .Select-value-label span');
-  //   expect(dimensionsUOM.getText()).toBe('cm');
-  // });
-  // it('defaults length to blank', () => {
-  //   const length = b.element('.length input');
-  //   expect(length.getValue()).toBe('');
-  // });
-  // it('defaults width to blank', () => {
-  //   const width = b.element('.width input');
-  //   expect(width.getValue()).toBe('');
-  // });
-  // it('defaults height to blank', () => {
-  //   const height = b.element('.height input');
-  //   expect(height.getValue()).toBe('');
-  // });
-  // it('defaults volume to 0', () => {
-  //   const volume = b.element('.volume input');
-  //   expect(volume.getValue()).toBe('0');
-  // });
-  // it('defaults weight UOM to kg', () => {
-  //   const weightUOM = b.element('.weight-uom .Select-value-label span');
-  //   expect(weightUOM.getText()).toBe('kg');
-  // });
-  // it('defaults weight total to 0', () => {
-  //   const weightTotal = b.element('.weight-total .non-editable-field-value');
-  //   expect(weightTotal.getText()).toBe('0');
-  // });
-  // it('defaults weight in kg to 0', () => {
-  //   const weightInUOM = b.element('.weight-in-kg .non-editable-field-value');
-  //   expect(weightInUOM.getText()).toBe('0');
-  // });
-  // it('defaults volume in CBM to 0', () => {
-  //   const volumeInCBM = b.element('.volume-in-cbm .non-editable-field-value');
-  //   expect(volumeInCBM.getText()).toBe('0');
-  // });
-  // it('defaults revenue tons to 0', () => {
-  //   const revenueTons = b.element('.revenue-tons .non-editable-field-value');
-  //   expect(revenueTons.getText()).toBe('0');
-  // });
-  // it('defaults hazardous material to false', () => {
-  //   const hazardousMaterial = b.element('.hazardous-material.checkbox');
-  //   expect(/active/.test(hazardousMaterial.getAttribute('class'))).toBe(false);
-  // });
-  // it('defaults household goods to false', () => {
-  //   const householdGoods = b.element('.household-goods.checkbox');
-  //   expect(/active/.test(householdGoods.getAttribute('class'))).toBe(false);
-  // });
-  // it('defaults preferred currency to USD', () => {
-  //   const preferredCurrency = b.element(
-  //     '.preferred-currency .Select-value-label span'
-  //   );
-  //   expect(preferredCurrency.getText()).toBe('USD - US Dollar');
-  // });
-  // it('defaults insurance required to false', () => {
-  //   const insuranceRequired = b.element('.insurance-required.checkbox');
-  //   expect(/active/.test(insuranceRequired.getAttribute('class'))).toBe(false);
-  // });
+  it('defaults movement type to Door to Door', async function() {
+    const text = await b.getText('.movement-type .Select-value-label span');
+    expect(text).toBe('Door to Door');
+  });
+  it('defaults a single package row', async function() {
+    const cargoRows = await b.elements('.package-row');
+    expect(cargoRows.value.length).toBe(1);
+  });
+  it('defaults package type to Packages', async function() {
+    const text = await b.getText('.package-type .Select-value-label span');
+    expect(text).toBe('Packages');
+  });
+  it('defaults quantity to 1', async function() {
+    const value = await b.getValue('.quantity input');
+    expect(value).toBe('1');
+  });
+  it('defaults to using dimensions', async function() {
+    const classes = await b.getAttribute('.dimensions .checkbox', 'class');
+    expect(/active/.test(classes)).toBe(true);
+  });
+  it('defaults dimensions UOM to cm', async function() {
+    const text = await b.getText('.dimensions-uom .Select-value-label span');
+    expect(text).toBe('cm');
+  });
+  it('defaults length to blank', async function() {
+    const value = await b.getValue('.length input');
+    expect(value).toBe('');
+  });
+  it('defaults width to blank', async function() {
+    const value = await b.getValue('.width input');
+    expect(value).toBe('');
+  });
+  it('defaults height to blank', async function() {
+    const value = await b.getValue('.height input');
+    expect(value).toBe('');
+  });
+  it('defaults volume to 0', async function() {
+    const value = await b.getValue('.volume input');
+    expect(value).toBe('0');
+  });
+  it('defaults weight UOM to kg', async function() {
+    const text = await b.getText('.weight-uom .Select-value-label span');
+    expect(text).toBe('kg');
+  });
+  it('defaults weight total to 0', async function() {
+    const text = await b.getText('.weight-total .non-editable-field-value');
+    expect(text).toBe('0');
+  });
+  it('defaults weight in kg to 0', async function() {
+    const text = await b.getText('.weight-in-kg .non-editable-field-value');
+    expect(text).toBe('0');
+  });
+  it('defaults volume in CBM to 0', async function() {
+    const text = await b.getText('.volume-in-cbm .non-editable-field-value');
+    expect(text).toBe('0');
+  });
+  it('defaults revenue tons to 0', async function() {
+    const text = await b.getText('.revenue-tons .non-editable-field-value');
+    expect(text).toBe('0');
+  });
+  it('defaults hazardous material to false', async function() {
+    const classes = await b.getAttribute(
+      '.hazardous-material.checkbox',
+      'class'
+    );
+    expect(/active/.test(classes)).toBe(false);
+  });
+  it('defaults household goods to false', async function() {
+    const classes = await b.getAttribute('.household-goods.checkbox', 'class');
+    expect(/active/.test(classes)).toBe(false);
+  });
+  it('defaults preferred currency to USD', async function() {
+    const text = await b.getText(
+      '.preferred-currency .Select-value-label span'
+    );
+    expect(text).toBe('USD - US Dollar');
+  });
+  it('defaults insurance required to false', async function() {
+    const classes = await b.getAttribute(
+      '.insurance-required.checkbox',
+      'class'
+    );
+    expect(/active/.test(classes)).toBe(false);
+  });
 });
